c4f004d1d7808e9a2742c30d8d95567ff9213387
Nisanthan <>
2019-10-30 00:11:12 -0400

    option to flip array of columns

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-const flip = (matrix) => {
-  for (let i = 0; i < matrix.length; i++) {
-    matrix[i] = matrix[i].reverse();
-  }
-  return matrix;
+const defaultOptions = {
+  arrayOfColumns: false
 };
-// O(N*M)
+
+const flip = (matrix = [], options = {}) => {
+  options = { ...defaultOptions, ...options };
+
+  if (options.arrayOfColumns) {
+    return matrix.reverse(); // O(N)
+  } else {
+    for (let i = 0; i < matrix.length; i++) {
+      matrix[i] = matrix[i].reverse();
+    }
+    return matrix; // O(N*M)
+  }
+};
+
 module.exports = flip;
